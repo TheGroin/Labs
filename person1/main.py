3bc510c65671519f0a8cb0df2027bc7c81f17ecf
TheGroin <veskan03@gmail.com>
2025-10-30 14:06:27 +0300

    feat(main): добавлен лог завершения обработки

--- person1/main.py
+++ person1/main.py
@@ -5,3 +5,5 @@ if __name__ == "__main__":
     data = read_data("input.txt")
     save_data("output.txt", data.upper())
     print("Данные успешно обработаны.")
+
+print("main: обработка завершена")
